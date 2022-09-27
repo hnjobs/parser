1b4d65f5b4e0a197b0afd1a6e0acd5ea4be9ad91
Emil Burzo <contact@emilburzo.com>
2022-09-27 21:48:43 +0300

    add ability to specify es host/port via env variable

--- src/main/java/com/emilburzo/hnjobs/main/Main.java
+++ src/main/java/com/emilburzo/hnjobs/main/Main.java
@@ -10,6 +10,9 @@ import java.net.UnknownHostException;
 
 public class Main {
 
+    private static final String ELASTICSEARCH_HOST = "ELASTICSEARCH_HOST";
+    private static final String ELASTICSEARCH_PORT = "ELASTICSEARCH_PORT";
+
     private static TransportClient client;
 
     public static void main(String[] args) {
@@ -24,8 +27,11 @@ public class Main {
         }
     }
 
-    private static void initEs() throws UnknownHostException {
-        client = TransportClient.builder().build().addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("hnjobs"), 9300));
+    private void initEs() throws UnknownHostException {
+        client = TransportClient.builder().build().addTransportAddress(new InetSocketTransportAddress(
+                InetAddress.getByName(System.getenv().getOrDefault(ELASTICSEARCH_HOST, "hnjobs")),
+                Integer.parseInt(System.getenv().getOrDefault(ELASTICSEARCH_PORT, "9300")))
+        );
     }
 
     public static TransportClient getClient() {
